b220392b64de3feb8137e0a7e2a0599b83a808f2
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-11-24 18:44:16 +0000

    conexão com o db

---
+++ src/database/ConexaoBD.java
@@ -0,0 +1,19 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexaoBD {
+
+    private static final String URL  = "jdbc:mysql://localhost:3306/dronesdb";
+    private static final String USER = "root";
+    private static final String PASS = "senha";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao conectar ao banco.", e);
+        }
+    }
+}
